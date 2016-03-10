32c2b5180399ba9050c3485475a13bc40100123b
jem <jem@melexis.com>
2016-03-10 22:54:45 +0100

    can give url as string

--- ArduinoCode/trialGet/trialGet.ino
+++ ArduinoCode/trialGet/trialGet.ino
@@ -6,35 +6,14 @@
 SoftwareSerial ESP8266(10,11);
 
 //wifi name and ssid to connect to:
-String nomWifi="********";
-String ssid="***********";
+String nomWifi="******";
+String ssid="***************";
 
-void sendRequestTrial1(){
-
-      
-     String startcommand = "AT+CIPSTART=\"TCP\",\"www.google.fr\", 80";
-     
-     ESP8266.println(startcommand);
-      delay(2000);
-     //the sendcommand string is empty as soon as there is a slash in the string
-     String sendcommand = "GET /analytics HTTP/1.1\r\nHost: www.google.com\r\n\r\n";//works for most cases
-      
-     
-     //debug the command
-     ESP8266.print("AT+CIPSEND=");
-     ESP8266.println(sendcommand.length());
-     delay(1000);
-     if (ESP8266.find(">")){
-     Serial.print("send command:");
-     Serial.println(sendcommand);
-      ESP8266.println(sendcommand);
-     }
-     recoitDuESP8266(5000);
-  
-}
 
 
 void setup() {
+
+  
   //set serial to see in serial monitor results:
   Serial.begin(9600);
   //set serial emulator to 115200 bauds:
@@ -62,21 +41,42 @@ void setup() {
   ESP8266.println("AT+CIPMUX=1");
   //should get OK:
   recoitDuESP8266(1000);
+  String request="google.com";
+       int slash = request.indexOf('/');
+     
+     //grab the domain
+     String domain;
+     if(slash>0){  
+       domain = request.substring(0,slash);
+     }else{
+       domain = request;
+     }
+
+     //get the path
+     String path;
+     if(slash>0){  
+       path = request.substring(slash);   
+     }else{
+       path = "/";          
+     }
+     
+
   //set on channel 4, TCP connection to google.com on port 80:
-  ESP8266.println("AT+CIPSTART=4,\"TCP\",\"google.com\",80");
+  ESP8266.println("AT+CIPSTART=4,\"TCP\",\""+domain+"\",80");
   //should get 4 connect and ok:
   recoitDuESP8266(1000);
+  
   //give amount of bytes to send to channel 4 (18 here):
-  ESP8266.println("AT+CIPSEND=4,18");
+  String getCommand="GET "+path+" HTTP/1.0\r\n\r\n";
+  ESP8266.print("AT+CIPSEND=4,");
+  //need to provide length apart else get issue with string concatenation:
+  ESP8266.println(getCommand.length());
   //should get ok and a prompt
   recoitDuESP8266(1000);
-  //send the request (the 18 bytes), the first slash is the path to resource:
-  ESP8266.println("GET / HTTP/1.0");
-  //send second carriage return:
-  ESP8266.println("");
+  //send the request not via println as CR/LN already in string to get correct length:
+  ESP8266.print(getCommand);
   //should get received 18 bytes and mentioned send ok and then receive the response and then 4,closed
   recoitDuESP8266(5000);
-  //sendRequestTrial1();
 
 }
 
@@ -87,14 +87,16 @@ void loop() {
 
 void recoitDuESP8266(const int timeout){
   long int time = millis();
-  String response="";
+  Serial.println("response:");
   while ((time+timeout)>millis()){
     
     while(ESP8266.available()){
         char c=ESP8266.read();
-        response+=c;
+        Serial.print(c);
+        
     }
   }
-  Serial.println("response:"+response);
+  Serial.println("");
+  
 
 }
